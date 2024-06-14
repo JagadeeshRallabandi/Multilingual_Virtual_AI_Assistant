cc1681cab35cd7d625f9563c5d13bb0e3c1b999b
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2024-06-14 17:55:34 +0530

    commit1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI_Project',
+    version= '0.0.1',
+    author= 'Jagadeesh Rallabandi',
+    author_email= 'rjagadeeshnit@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
